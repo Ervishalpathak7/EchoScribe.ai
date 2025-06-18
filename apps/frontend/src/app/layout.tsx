b486dfa96a0158583a6749733e8e8e2b6bc19c19
Vishal Pathak <vishalpathak.er@gmail.com>
2025-06-18 20:36:55 +0530

    fix layout left alligned issue

--- apps/frontend/src/app/layout.tsx
+++ apps/frontend/src/app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
     <html lang="en" suppressHydrationWarning>
       <body className={inter.className}>
         <ThemeProvider attribute="class" defaultTheme="light" enableSystem={false} disableTransitionOnChange>
-          <div className="flex min-h-screen flex-col">
+          <div className="flex min-h-screen flex-col items-center ">
             <Header />
             <main className="flex-1">{children}</main>
           </div>
